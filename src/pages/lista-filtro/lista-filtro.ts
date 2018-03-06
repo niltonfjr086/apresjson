e9e8489b340599b79f36658011c4191be21f280e
Nilton F Jr <>
2018-03-06 18:28:23 -0300

    confeccionando modal

--- src/pages/lista-filtro/lista-filtro.ts
+++ src/pages/lista-filtro/lista-filtro.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { Filtro } from '../../providers/basejson/filtrojson';
+import { BasejsonProvider } from '../../providers/basejson/basejson';
 
 /**
  * Generated class for the ListaFiltroPage page.
@@ -15,15 +17,50 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class ListaFiltroPage {
 
-  constructor(public navCtrl: NavController, private navParams: NavParams, private view: ViewController) {
+  private filtrojson: Filtro;
+
+  private nomePrograma: string;
+  private ano: number;
+  private municipio: string;
+
+  constructor(public navCtrl: NavController, private basejson: BasejsonProvider,
+    private navParams: NavParams, private view: ViewController) {
   }
 
-  closeModal() {
+  public closeModal() {
     this.view.dismiss();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ListaFiltroPage');
+  }
+
+  public montarPeloFiltro() {
+    this.montarFiltro();
+    console.log(this.filtrojson.valorBusca);
+    console.log(this.filtrojson.campoBusca)
+    this.basejson.listarPorFiltro(this.filtrojson).then(
+      () => {
+        console.log(this.basejson.lista);
+      }
+    );
+  }
+
+  private montarFiltro() {
+    this.filtrojson = new Filtro();
+
+    if (this.nomePrograma) {
+      this.filtrojson.campoBusca.push('nom_programa');
+      this.filtrojson.valorBusca.push(this.nomePrograma);
+    }
+    if (this.ano) {
+      this.filtrojson.campoBusca.push('ano');
+      this.filtrojson.valorBusca.push(this.ano);
+    }
+    if (this.municipio) {
+      this.filtrojson.campoBusca.push('municipio');
+      this.filtrojson.valorBusca.push(this.municipio);
+    }
   }
 
 }
