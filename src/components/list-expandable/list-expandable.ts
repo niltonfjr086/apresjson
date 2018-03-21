33bcdbcf340a40f128ae936f50fd6d2e030e70ca
Nilton F Jr <>
2018-03-21 18:32:03 -0300

    autocomplete funcional ok

--- src/components/list-expandable/list-expandable.ts
+++ src/components/list-expandable/list-expandable.ts
@@ -9,6 +9,9 @@ import { ViewController } from 'ionic-angular';
 })
 export class ListExpandableComponent {
 
+  private listaFlutuanteMunicipio = [];
+  private focus = false;
+
   @ViewChild('expandWrapper', { read: ElementRef }) expandWrapper;
   @Input('expanded') expanded;
 
@@ -17,7 +20,7 @@ export class ListExpandableComponent {
   }
 
   limpar() {
-    for(let b in this.provider._filter){
+    for (let b in this.provider._filter) {
       this.provider._filter[b] = null;
     }
     this.listaPage.situacao = "Filtro Limpo";
@@ -28,6 +31,26 @@ export class ListExpandableComponent {
   buscar() {
     this.listaPage.situacao = "Filtro Preenchido";
     this.listaPage.expand();
+  }
+
+  toggleFocus() {
+    this.focus = !this.focus;
+
+    if (this.focus) {
+      this.provider._filter.municipio = "";
+      this.search();
+    } else {
+
+    }
+
+  }
+  search() {
+    this.listaFlutuanteMunicipio = this.provider.viewSelects['municipio']
+      .filter((f, i, ff) => { return ff[i].toLowerCase().includes(this.provider._filter.municipio.toLowerCase()) });
+  }
+  seleciona(res) {
+    this.focus = !this.focus;
+    this.provider._filter.municipio = res;
   }
 
 }
